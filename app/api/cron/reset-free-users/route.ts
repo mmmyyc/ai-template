280ddab056b474dc26dd14cbcdf6d1599fd35445
mmmyyc <mmmmmyyc@gmail.com>
2025-01-30 15:57:41 +0800

    重构代码

--- app/api/cron/reset-free-users/route.ts
+++ app/api/cron/reset-free-users/route.ts
@@ -1,7 +1,5 @@
-import { createServerClient } from "@supabase/ssr";
-import { cookies } from "next/headers";
 import { NextResponse } from 'next/server';
-
+import { createAdminClient } from "@/libs/supabase/server";
 export const dynamic = 'force-dynamic';
 
 export async function GET(request: Request) {
@@ -12,31 +10,7 @@ export async function GET(request: Request) {
         return new Response('Unauthorized', { status: 401 });
     }
 
-    // 使用 service_role 创建客户端
-    const cookieStore = cookies();
-    const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.SUPABASE_SERVICE_ROLE_KEY!, // 使用 service_role key
-      {
-        cookies: {
-          getAll() {
-            return cookieStore.getAll();
-          },
-          setAll(cookiesToSet) {
-            try {
-              cookiesToSet.forEach(({ name, value, options }) =>
-                cookieStore.set(name, value, options)
-              );
-            } catch {
-              // The `setAll` method was called from a Server Component.
-              // This can be ignored if you have middleware refreshing
-              // user sessions.
-            }
-          },
-        },
-      }
-    );
-
+    const supabase = createAdminClient();
     // 执行重置操作
     const { error } = await supabase
       .from('profiles')
