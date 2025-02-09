bfcca9d00a74ae1b2c3d6307502075b0fb704da6
MYC <2536346598@qq.com>
2025-02-09 22:00:10 +0800

    简单修改一下

--- app/signin/layout.tsx
+++ app/signin/layout.tsx
@@ -1,12 +1,12 @@
 import { ReactNode } from "react";
 import config from "@/config";
 import { getSEOTags } from "@/libs/seo";
-
+import { Suspense } from 'react'
 export const metadata = getSEOTags({
   title: `Sign-in to ${config.appName}`,
   canonicalUrlRelative: "/auth/signin",
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
+  return <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>;
 }
